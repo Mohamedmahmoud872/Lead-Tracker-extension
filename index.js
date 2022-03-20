798ac59eed794776a96a4f2439be911728c10da8
unknown <mohamedmahmoud6524@gmail.com>
2022-03-20 06:51:40 +0200

    First version of url tracking extension

---
+++ index.js
@@ -0,0 +1,53 @@
+/*
+json.stringify(array) --> array to string
+json.parse(array) --> string to array
+*/
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if(leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  rendering(myLeads)
+}
+
+tabBtn.addEventListener("click", function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    rendering(myLeads)
+  })
+})
+
+function rendering(leads){
+  let listItems = ""
+  for (var i = 0; i < leads.length; i++) {
+    listItems += `<li>
+                    <a target='_blank' href='${leads[i]}'>
+                      ${leads[i]}
+                    </a>
+                  </li>`
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+  }
+  ulEl.innerHTML = listItems
+}
+
+deleteBtn.addEventListener("dblclick", function(){
+  localStorage.clear()
+  myLeads = []
+  rendering(myLeads)
+})
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  rendering(myLeads)
+  console.log(localStorage.getItem("myLeads"))
+})
